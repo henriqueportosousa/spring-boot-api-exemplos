b46af1d199b94bed2fc0daff6902834dd249246a
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-08-28 10:33:19 -0300

    Aula de TPI2

---
+++ src/main/java/br/com/fatec/apiexemplousuario/model/Usuario.java
@@ -0,0 +1,30 @@
+package br.com.fatec.apiexemplousuario.model;
+
+public class Usuario {
+    private String nome;
+    private int idade;
+
+    public Usuario() {}
+
+    public Usuario(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
+
