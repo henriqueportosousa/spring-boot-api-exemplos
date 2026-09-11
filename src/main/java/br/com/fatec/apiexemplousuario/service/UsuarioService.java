50d96e234c7f3c7e12a76456d8ac4c1deeda98a1
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-09-11 09:51:56 -0300

    integração com H2

--- src/main/java/br/com/fatec/apiexemplousuario/service/UsuarioService.java
+++ src/main/java/br/com/fatec/apiexemplousuario/service/UsuarioService.java
@@ -1,51 +1,43 @@
 package br.com.fatec.apiexemplousuario.service;
 
 import br.com.fatec.apiexemplousuario.model.Usuario;
+import br.com.fatec.apiexemplousuario.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
-    private final ArrayList<Usuario> listaUsuarios = new ArrayList<>();
 
-    //listar todos
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     public List<Usuario> listar() {
-        return listaUsuarios;
+        return usuarioRepository.findAll();
     }
 
-    //buscar por Índice
-    public Usuario buscarPorIndice(int indice) {
-        if (indice < 0 || indice >= listaUsuarios.size()) {
-            return null;
-        }
-
-        return listaUsuarios.get(indice);
+    public Optional<Usuario> buscarPorId(Integer id) {
+        return usuarioRepository.findById(id);
     }
 
-    // adicionar
-    public Usuario adicionar(Usuario usuario) {
-        listaUsuarios.add(usuario);
-        return usuario;
+    public Optional<Usuario> atualizar(Integer id, Usuario usuarioAtualizado) {
+        return usuarioRepository.findById(id).map(usuarioExistente -> {
+            usuarioExistente.setNome(usuarioAtualizado.getNome());
+            usuarioExistente.setIdade(usuarioAtualizado.getIdade());
+            return usuarioRepository.save(usuarioExistente);
+        });
     }
 
-    //atualizar
-    public Usuario atualizar(int indice, Usuario usuario) {
-        if (indice < 0 || indice >= listaUsuarios.size()) {
-            return null;
-        }
-
-        listaUsuarios.set(indice,usuario);
-        return usuario;
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
     }
 
-    public boolean deletar(int indice) {
-        if (indice < 0 || indice >= listaUsuarios.size()) {
-            return false;
-        }
-
-        listaUsuarios.remove(indice);
-        return true;
+    public void deletar(Integer id) {
+        usuarioRepository.deleteById(id);
     }
 }
